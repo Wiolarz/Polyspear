1d704bacc8d808040cfae8c1d9e229d49325e85f
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2023-06-26 22:28:12 +0200

    early ui experiments

--- Kings_Hunt/Controller_System.gd
+++ Kings_Hunt/Controller_System.gd
@@ -1,21 +1,41 @@
-extends Node
-
-class_name Controller_System
-
-
-
-@export var card_system : Card_System
-
-
-@export var input : Node
-
-
-
-
-
 '''
 sends an input system informations regarding card system state
 
 then receives player choice and sends information toward card system and 
 main node which will inform the game manager
 '''
+
+extends Node
+
+class_name Controller_System
+
+@export var action_buttons_container : HBoxContainer
+@export var Cards_Container : VBoxContainer
+
+
+@export var card_system : Card_System
+
+@export var input : Node
+
+var current_choice = "hide"
+
+var player_controller = false
+
+func _ready():
+	if input.is_class("player_input"):
+		player_controller = true
+	
+	input.connect("card", card_choice)
+	input.connect("action", action_choice)
+
+
+func card_choice(card_id):
+	if player_controller:
+		pass # attempt to highlight the card
+	pass
+
+func action_choice(action_name):
+	pass
+
+func turn_reset():
+	current_choice = "hide"
