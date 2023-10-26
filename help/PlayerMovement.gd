594c551e136c8468f52c2c14c2f79efb0175879b
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2023-10-26 22:10:15 +0200

    alpha

---
+++ help/PlayerMovement.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+
+@export var speed = 4
+
+
+func get_input():
+	
+	
+	
+	linear_velocity += transform.y * Input.get_axis("KEY_UP", "KEY_DOWN") * speed
+
+	linear_velocity += transform.x * Input.get_axis("KEY_LEFT", "KEY_RIGHT") * speed
+
+
+
+func _physics_process(_delta):
+
+	get_input()
+	
