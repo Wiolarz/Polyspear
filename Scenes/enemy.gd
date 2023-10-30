3860409f66d9cfed918890374ad3bef7eb3d50e0
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2023-10-30 21:02:41 +0100

    finished AI

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -9,7 +9,7 @@ extends Node2D
 @onready var rotate_tool = $Weapon/WeaponBody
 
 
-@export var speed = 2
+@export var speed = 8
 
 
 @export var player : Node2D
@@ -20,29 +20,52 @@ func _ready():
 
 func _physics_process(_delta):
 	
-	#direction
-	var p_direction = fmod(rad_to_deg(position.angle_to_point(player.global_position)) + 360, 360) # - 360
+	var p_direction = fmod(rad_to_deg(move_tool.global_position.angle_to_point(player.global_position)) + 360, 360) # - 360
 	var current_rotation = fmod(rotate_tool.rotation_degrees + 360, 360)
 	var goal_direction = p_direction - current_rotation
-	#if goal_direction > 300:
-		#print(goal_direction)
+	goal_direction = fmod(goal_direction + 360, 360)
+	
+	if goal_direction > 180:
+		goal_direction -= 360
 
 	print(goal_direction, "  ", p_direction, "   ", current_rotation)
-	
-	#var cos = 360 - p_direction 
-	if goal_direction > 180:
-		goal_direction *= -1	
-	#if goal_direction + cos > abs(current_rotation - goal_direction):
-	#	goal_direction *= -1
-	
+
+
 	rotate_tool.direction_change(clamp(goal_direction, -1, 1))
+	
+	
+	
+	
+	
+	
+	#direction
+#	var player_direction = fmod(rad_to_deg(move_tool.global_position.angle_to_point(player.global_position)) + 360, 360)
+#	var current_rotation = fmod(rotate_tool.rotation_degrees + 360, 360)
+#	var goal_angle = player_direction - current_rotation
+#	#if goal_direction > 300:
+#		#print(goal_direction)
+#
+#	print(goal_angle, "  ", player_direction, "   ", current_rotation)
+#
+#
+#
+##	if fmod(360.0 + goal_angle - current_rotation, 360.0) > 180.0:
+##		goal_angle *= -1
+#
+#	var cos = 360 - player_direction
+#	if goal_angle > 300 and goal_angle > cos:
+#		goal_angle *= -1
+#
+#
+#	#if goal_direction + cos > abs(current_rotation - goal_direction):
+#	#	goal_direction *= -1
+#
+#	rotate_tool.direction_change(clamp(goal_angle, -1, 1))
 
 
 	# movment
-	#move_tool.linear_velocity += move_tool.transform.y * clamp(player.global_position.x - move_tool.global_position.x, -1, 1) * speed
-
-	#move_tool.linear_velocity += move_tool.transform.x * clamp(player.global_position.y - move_tool.global_position.y, -1, 1) * speed
-
+	var pdir = (player.global_position - move_tool.global_position).normalized() * speed
+	move_tool.move_and_collide(pdir)
 
 
 
