cee23e11b88425821819ba6f315c45d34048a978
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2023-11-13 20:02:51 +0100

    added font file + small change of file structure in art folder

--- help/List_of_Useful_Links.gd
+++ help/List_of_Useful_Links.gd
@@ -1,1 +1,23 @@
+extends Node
 # List of Useful Links
+
+
+
+var AI
+"""
+
+"""
+
+
+
+
+var UI
+"""
+
+General UI - 3:33:26 hour
+https://youtu.be/nAh_Kx5Zh5Q?si=KISCRiJ_DlFkDb7K&t=27206
+
+fonts
+https://docs.godotengine.org/en/stable/tutorials/ui/gui_using_fonts.html
+
+"""
