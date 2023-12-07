bca17af59f2ae708297f09cdf9aeefc10b7d2080
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2023-12-07 20:34:05 +0100

    Readme + TODO update

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Krong season 2
 Organised by Wiolarz a regular meeting with people interested in learning Godot.
 This time each meeting will have it's own separete branch
+
+# Shmup
+Space themed Shootem'up tilted "Spejz 3", sequal to Spejz 2 https://github.com/BartDrown/polygon-21z/tree/develop
+
+
+# Contributors
+Wiolarz - Młody
+MrTrupen - Trupen
+Carcinizer
--- help/TODO.gd
+++ help/TODO.gd
@@ -1,10 +1,35 @@
 # TODO
 
+var GOAL_FOR_NEXT_KRONG
+"""
+Add cheat system based on F3 F4 system
 
+
+SHOTTING
+
+BASIC ENEMY AI
+
+UPGRADES
+
+
+1 Random enemy attacks
+2 enemy spawn as waves
+3 waves generator (special 'map' creation tool)
+
+
+I Progression system: money + rare upgrade drops
+
+"""
 
 
 var ADD
 """
+
+
+
+
+
+
 Create component framework for this project ex.
 Turret basic class
 basic frame -- art
@@ -15,11 +40,16 @@ Movement type - rotation speed behavior
 
 various ammo types and system around those
 
+
+ammo cannot hit your own ship
+
 """
 
 
 var CHANGE
 """
+
+Bullets should retain ship starting velocity
 
 Change turn rate of turret to a value (instead of instant)
 
