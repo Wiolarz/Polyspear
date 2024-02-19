8622d6faad0d1d1046f788b704c0774fed5fdf63
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2024-02-19 03:04:37 +0100

    small documentation update
    Bigger Units upgrade described

--- Documentation/TODO.gd
+++ Documentation/TODO.gd
@@ -1,5 +1,22 @@
 # TODO
 
+
+var FIX_ASAP  # game breaking BUGS
+"""
+
+"""
+
+var FIX # Stuff that needs to be fixed, for the game to work properly
+"""
+In gameplay manager setting up units if one player has already placed all of their units
+
+"""
+
+
+
+
+
+
 var GOAL
 """
 
@@ -18,4 +35,25 @@ var CHANGE
 
 
 """
+
+
+#region Bigger Units
+
+# Foreword
+"""
+The addition of bigger sized units will be hard to implement, while in the end in may be a bad feature.
+
+Its still worthwile to document stuff needed to be changed once the work on this feature would start.
+"""
+
+
+"""
+Graphics/Technical -> Spacing_of_the_Grid
+
+
+"""
+
+
+#endregion
+
 
