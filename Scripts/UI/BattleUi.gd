9d4b45cb23cc2ece51cb8d5d62a80ae7180f5c13
NamespaceV <s-zephyr@o2.pl>
2024-03-30 23:11:16 +0100

    uio for placing units before battle starts wip

---
+++ Scripts/UI/BattleUi.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready
+var playersBox : BoxContainer = $Players
+
+@onready
+var unitsBox : BoxContainer = $Units
+
+func _ready():
+    var players = playersBox.get_children()
+    players[2].queue_free()
+    players[1].queue_free()
+    var n = Button.new()
+    n.text = "Player 1"
+    n.pressed.connect(func p1(): onPlayerSelect(1))
+    playersBox.add_child(n)
+    var n2 = Button.new()
+    n2.text = "Player 2"
+    n2.pressed.connect(func p1(): onPlayerSelect(2))
+    playersBox.add_child(n2)
+
+func onPlayerSelect(playerId:int):
+    for c in unitsBox.get_children():
+        c.queue_free()
+    var b = TextureButton.new()
+    b.texture_normal = load( "res://Art/elf1.png" if playerId == 1 else  "res://Art/ork1.png" )
+    b.add_child(load("res://Scenes/Units/elf/Archer.tscn").instantiate())
+    unitsBox.add_child(b)
+
