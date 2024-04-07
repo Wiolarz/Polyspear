6dc54821bca486cdf2f6b1c0f4a11a47f68508c4
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2024-04-07 22:45:12 +0200

    removed old symbol scenes + added smaller window size for testing

--- Scripts/UI/fsave.gd
+++
@@ -1,6 +0,0 @@
-class_name Save
-extends Resource
-
-
-#@export var player_position: Vector2
-#@export var player_health: int
--- Scripts/UI/save_system.gd
+++ Scripts/UI/save_system.gd
@@ -3,31 +3,13 @@ extends Node
 @export var reset_save = false
 
 
-var player_file = "user://save.tres"
-
-
-func load_game():
-	var save = load(player_file)
-	if save:
-		BUS.load_game.emit(save)
-
-func save_game():
-	var save = Save.new()
-	BUS.collect_save_data.emit(save)
-	#print(save)
-	var info = ResourceSaver.save(save, player_file)
-	if info != OK:
-		print(info)
-	#print("saved game")
-
-
 """
 Save system could be deactivated by the GAME_MENU Manager
 To allow saving only after entering the menu, while the game is paused
 """
 func _process(_delta):
 	if Input.is_action_just_pressed("KEY_SAVE_GAME"):
-		save_game()
+		print("quick save is not yet supported")
 	
 	if Input.is_action_just_pressed("KEY_LOAD_GAME"):
-		load_game()
+		print("quick load is not yet supported")
