c10208ba86d363ba04dcd6b1d9b1bcb3c6ca678a
NamespaceV <s-zephyr@o2.pl>
2024-04-11 02:26:27 +0200

    add mouse drag on middle pressed

--- Scripts/General/camera.gd
+++ Scripts/General/camera.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+var last_mouse_position:Vector2
+
 func _process(_delta):
 	
 	if Input.is_action_just_pressed("KEY_ZOOM_OUT"):
@@ -11,6 +13,14 @@ func _process(_delta):
 		if zoom.x < 1:
 			zoom.x += 0.1
 			zoom.y += 0.1
+			
+	if Input.is_action_pressed("KEY_DRAG_CAMERA"):
+		var current_mouse_position = get_viewport().get_mouse_position()
+		if Input.is_action_just_pressed("KEY_DRAG_CAMERA"):
+			last_mouse_position = current_mouse_position
+		var diff = current_mouse_position - last_mouse_position
+		position -= diff / zoom
+		last_mouse_position = current_mouse_position
 
 	var input_direction = Input.get_vector("KEY_LEFT", "KEY_RIGHT", "KEY_UP", "KEY_DOWN")
 	if input_direction.x < 0:
