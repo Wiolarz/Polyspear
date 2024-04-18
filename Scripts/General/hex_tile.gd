4eb44b048e418877c5c125695be002be9e10504a
NamespaceV <s-zephyr@o2.pl>
2024-04-18 11:58:59 +0200

    Improved drawing on map editor (#14)
    Co-authored-by: Wiolarz <36847708+Wiolarz@users.noreply.github.com>

--- Scripts/General/hex_tile.gd
+++ Scripts/General/hex_tile.gd
@@ -7,7 +7,7 @@ var cord : Vector2i
 var type : String = "sentinel"
 
 func _on_input_event(_viewport:Node, event:InputEvent, _shape_idx:int):
-	if event.is_action_pressed("KEY_SELECT"):
+	if Input.is_action_pressed("KEY_SELECT"):
 		IM.grid_input_listener(cord)
 	
 
