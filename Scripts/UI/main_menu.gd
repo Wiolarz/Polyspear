5af8cc64a93af77329b5b3ae487bf9b63229741e
NamespaceV <s-zephyr@o2.pl>
2024-04-28 12:45:32 +0200

    remove old battle setup

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,23 +1,5 @@
 extends CanvasLayer
 
-func _on_editors_menu_id_pressed(id):
-	match id:
-		0: _on_map_editor_pressed()
-		1: _on_unit_editor_pressed()
-		_: pass
-
-
-func _on_unit_editor_pressed():
-	UI.go_to_unit_editor()
-
-
-func _on_map_editor_pressed():
-	UI.go_to_map_editor()
-
-
-func _on_battle_setup_pressed():
-	UI.go_to_test_battle_setup()
-
 
 func _on_host_pressed():
 	UI.go_to_host_lobby()
@@ -33,3 +15,18 @@ func _on_replay_pressed():
 
 func _on_file_dialog_replay_file_selected(path):
 	BM.load_replay(path)
+
+
+func _on_editors_menu_id_pressed(id):
+	match id:
+		0: _on_map_editor_pressed()
+		1: _on_unit_editor_pressed()
+		_: pass
+
+
+func _on_unit_editor_pressed():
+	UI.go_to_unit_editor()
+
+
+func _on_map_editor_pressed():
+	UI.go_to_map_editor()
--- Scripts/UI/ui_manager.gd
+++ Scripts/UI/ui_manager.gd
@@ -3,14 +3,12 @@ extends Node
 
 var in_game_menu
 var main_menu
-var test_battle_setup
 var map_editor
 var unit_editor
 var host_lobby
 var client_lobby
 
 func _ready():
-	test_battle_setup = load("res://Scenes/UI/Lobby/BattleSetup.tscn").instantiate()
 	in_game_menu = load("res://Scenes/UI/GameMenu.tscn").instantiate()
 	main_menu    = load("res://Scenes/UI/MainMenu.tscn").instantiate()
 	map_editor   = load("res://Scenes/UI/Editors/MapEditor.tscn").instantiate()
@@ -19,7 +17,6 @@ func _ready():
 	client_lobby = load("res://Scenes/UI/Lobby/ClientLobby.tscn").instantiate()
 
 	add_child(main_menu)
-	add_child(test_battle_setup)
 	add_child(map_editor)
 	add_child(unit_editor)
 	add_child(host_lobby)
@@ -28,13 +25,16 @@ func _ready():
 
 	_hide_all()
 
+
 func add_custom_screen(custom_ui : CanvasLayer):
 	add_child(custom_ui)
 	custom_ui.hide()
 
+
 func go_to_custom_ui(custom_ui : CanvasLayer):
 	_hide_all()
 	custom_ui.show()
+
 
 func _hide_all():
 	for c in get_children():
@@ -50,25 +50,26 @@ func go_to_unit_editor():
 	_hide_all()
 	unit_editor.show()
 
+
 func go_to_map_editor():
 	IM.draw_mode = true
 	_hide_all()
 	map_editor.open_draw_menu()
 
-func go_to_test_battle_setup():
-	_hide_all()
-	test_battle_setup.show()
 
 func go_to_host_lobby():
 	_hide_all()
 	host_lobby.show()
 
+
 func go_to_client_lobby():
 	_hide_all()
 	client_lobby.show()
 
+
 func show_in_game_menu():
 	in_game_menu.show()
+
 
 func hide_in_game_menu():
 	in_game_menu.hide()
