842060ddc65fbfb57dfe32ee68f9dd67863cf653
NamespaceV <s-zephyr@o2.pl>
2024-04-29 15:00:57 +0200

    comment GameSetupInfo

--- Scripts/General/game_setup_info.gd
+++ Scripts/General/game_setup_info.gd
@@ -1,25 +1,17 @@
 class_name GameSetupInfo
 extends RefCounted
-# not a resource because we refer to players, sessions and other temporary
-# things
 
-class Slot extends RefCounted: # check if this is good base
+## Game Setup in the lobby, currently used only in multiplayer [br]
+## Map chosen, info on game slots setup (factions, colors, etc) [br]
+## TODO: start using in single player [br]
+## TODO: synch slot count etc with map when changing maps [br]
+## WARNING not a resource because we refer to players, sessions
+## and other temporary objects that should not be saved
 
-	## identifies who used this slot
-	## int or String
-	## String == "" -> we (local player)
-	## String != "" -> remote player
-	## int -> AI level eg. 1
-	var occupier = 0
-
-	var faction : DataFaction = null
-	var color : int = 0 # index of color in input manager
-	var army_set : PresetArmy = null # unused in scenario
-
-
-var slots : Array[Slot]
+# var mode_is_battle : bool = false
 # var battle_map : DataBattleMap # used only in battle game
 var world_map : DataWorldMap # used only in full world game
+var slots : Array[Slot]
 
 
 func to_dictionary(local_username : String = "") -> Dictionary:
@@ -57,8 +49,10 @@ static func from_dictionary(dict : Dictionary, \
 
 
 static func occupier_prepare_for_network(occupier, local_username : String):
-	return local_username if (occupier is String and occupier == "") \
-		else occupier
+	if occupier is String and occupier == "":
+		return local_username
+	return occupier
+
 
 static func occupier_receive_from_network(occupier, local_username : String):
 	if occupier is String and occupier == local_username:
@@ -66,3 +60,21 @@ static func occupier_receive_from_network(occupier, local_username : String):
 	if occupier is String or occupier is int:
 		return occupier
 	push_error("invalid occupier received ", occupier)
+
+
+## Info for a single slot
+class Slot extends RefCounted: # check if this is good base
+
+	## occupier identifies who uses this slot [br]
+	## its an `int` or a `String` [br]
+	## `String == ""` -> we (local player) [br]
+	## `String != ""` -> remote player with specified network name [br]
+	## `int` -> AI level eg. 1
+	var occupier = 0
+
+	## index of color see `CFG.TEAM_COLORS`
+	var color : int = 0
+
+	var faction : DataFaction = null
+
+	## TODO: add army setup for single battle
