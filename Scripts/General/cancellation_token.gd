1a9b3f395c946e3182fed1a801602dcf034768bf
NamespaceV <s-zephyr@o2.pl>
2024-07-11 15:04:15 +0200

    link to C# doc

--- Scripts/General/cancellation_token.gd
+++ Scripts/General/cancellation_token.gd
@@ -2,6 +2,7 @@ class_name CancellationToken
 extends Node
 
 ## Simple object that can be used to cancel long running opoerations with awaits etc.
+## see https://learn.microsoft.com/en-us/dotnet/standard/parallel-programming/task-cancellation
 
 var is_cancelled : bool
 
