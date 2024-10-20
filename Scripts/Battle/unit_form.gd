3f0575a4aa2958d8bdefe6b1c9e6dd348d2358ce
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2024-10-20 17:31:22 +0200

    unit sprites flip toward the movement direction

--- Scripts/Battle/unit_form.gd
+++ Scripts/Battle/unit_form.gd
@@ -19,6 +19,18 @@ var _play_death_anim : bool
 var _symbols_flipped : bool = true  # flag used for unit rotation
 
 
+func _physics_process(delta):
+	if entity:
+		$Symbols.modulate = Color.RED if entity.is_on_swamp else Color.WHITE
+	if _animate_rotation():
+		return
+	if _animate_movement():
+		return
+	_animate_death(delta)
+
+
+#region Init
+
 static func create(new_unit : Unit) -> UnitForm:
 	var result = CFG.UNIT_FORM_SCENE.instantiate()
 	result.name = new_unit.template.unit_name
@@ -37,7 +49,9 @@ static func create(new_unit : Unit) -> UnitForm:
 	result.rotation_degrees = new_unit.unit_rotation * 60
 	result._target_rotation_degrees = result.rotation_degrees
 	result.get_node("sprite_unit").rotation = -result.rotation
+	result.get_node("RigidUI").rotation = -result.rotation
 	return result
+
 
 ## HACK, this is for visuals only for summon UI
 ## no underlying Unit exists
@@ -47,14 +61,111 @@ static func create_for_summon_ui(template: DataUnit, color : DataPlayerColor) ->
 	return result
 
 
-func _physics_process(delta):
-	if entity:
-		$Symbols.modulate = Color.RED if entity.is_on_swamp else Color.WHITE
-	if _animate_rotation():
+func apply_graphics(template : DataUnit, color : DataPlayerColor):
+	var unit_texture = load(template.texture_path) as Texture2D
+	_apply_unit_texture(unit_texture)
+	_apply_color_texture(color)
+	_apply_level_number(template.level)
+	for dir in range(0,6):
+		var symbol_texture = template.symbols[dir].texture_path
+		_apply_symbol_sprite(dir, symbol_texture)
+	
+	_flip_unit_sprite()
+
+
+## WARNING: called directly in UNIT EDITOR
+func _apply_symbol_sprite(dir : int, texture_path : String) -> void:
+	var symbol_sprite = $"Symbols".get_children()[dir].get_child(0).get_child(0)
+	if texture_path == null or texture_path.is_empty():
+		symbol_sprite.texture = null
+		symbol_sprite.hide()
 		return
-	if _animate_movement():
-		return
-	_animate_death(delta)
+	symbol_sprite.texture = load(texture_path)
+
+	_flip_symbol_sprite(symbol_sprite, dir)
+
+	symbol_sprite.show()
+
+
+## Flips ths sprite so that weapons always point to the top of the screen
+func _flip_symbol_sprite(symbol_sprite : Sprite2D, dir : int):
+	var abstract_rotation : int = 0
+	if entity != null:
+		abstract_rotation = (entity.unit_rotation + dir) % 6
+	if abstract_rotation in [0, 1, 5]:  # LEFT
+		symbol_sprite.flip_h = false
+	else:
+		symbol_sprite.flip_h = true
+
+func _flip_unit_sprite():
+	var abstract_rotation : int = 0
+	if entity != null:
+		abstract_rotation = (entity.unit_rotation) % 6
+	if abstract_rotation in [0, 1, 5]:  # LEFT
+		$sprite_unit.flip_h = false
+	else:
+		$sprite_unit.flip_h = true
+
+## WARNING: called directly in UNIT EDITOR
+func _apply_unit_texture(texture : Texture2D) -> void:
+	$sprite_unit.texture = texture
+
+
+func _apply_color_texture(color : DataPlayerColor) -> void:
+	var color_texture_name : String = color.hexagon_texture
+	var path = "res://Art/player_colors/%s.png" % color_texture_name
+	var texture = load(path) as Texture2D
+	assert(texture, "failed to load background " + path)
+	$sprite_color.texture = texture
+
+
+func _apply_level_number(level : int) -> void:
+	const roman_numbers = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
+	if level > 10 or level < 0:
+		assert(false, "Design wise higher level units don't make sense")
+		level = 1
+	$RigidUI/UnitLevel.text = roman_numbers[level]
+
+#endregion Init
+
+
+#region Instant Animation Mode
+## Used only when animation speed is set to instant
+
+func update_movement_immediately():
+	var tile_position = BM.get_tile_global_position(entity.coord)
+	global_position = tile_position
+
+
+func update_turn_immediately():
+	var side = entity.unit_rotation
+	_target_rotation_degrees = (60 * (side))
+	rotation_degrees = _target_rotation_degrees
+	$sprite_unit.rotation = -rotation
+	$RigidUI.rotation = -rotation
+	_rotation_symbol_flip()
+
+
+func update_death_immediately():
+	# TODO maybe check if unit is dead??
+	scale = Vector2(0.0, 0.0)
+
+#endregion Instant Animation Mode
+
+
+#region Animations
+
+func _rotation_symbol_flip():
+	#TEMP placed here
+	_flip_unit_sprite()
+
+	_symbols_flipped = true
+
+	for dir in range(6):
+		var symbol_sprite = $"Symbols".get_children()[dir].get_child(0).get_child(0)
+		if symbol_sprite.texture == null:
+			continue
+		_flip_symbol_sprite(symbol_sprite, dir)
 
 
 func start_turn_anim():
@@ -82,34 +193,6 @@ func start_death_anim():
 	_play_death_anim = true
 
 
-func update_movement_immediately():
-	var tile_position = BM.get_tile_global_position(entity.coord)
-	global_position = tile_position
-
-
-func update_turn_immediately():
-	var side = entity.unit_rotation
-	_target_rotation_degrees = (60 * (side))
-	rotation_degrees = _target_rotation_degrees
-	$sprite_unit.rotation = -rotation
-	_rotation_symbol_flip()
-
-
-func update_death_immediately():
-	# TODO maybe check if unit is dead??
-	scale = Vector2(0.0, 0.0)
-
-
-func _rotation_symbol_flip():
-	_symbols_flipped = true
-
-	for dir in range(6):
-		var symbol_sprite = $"Symbols".get_children()[dir].get_child(0).get_child(0)
-		if symbol_sprite.texture == null:
-			continue
-		_flip_symbol_sprite(symbol_sprite, dir)
-
-
 func _animate_rotation() -> bool:
 	if not _play_turn_anim:
 		_symbols_flipped = false
@@ -121,6 +204,7 @@ func _animate_rotation() -> bool:
 	if CFG.animation_speed_frames == CFG.AnimationSpeed.INSTANT:
 		rotation_degrees = _target_rotation_degrees
 		$sprite_unit.rotation = -rotation
+		$RigidUI.rotation = -rotation
 		print("instant turn end")
 		anim_end.emit()
 		_play_turn_anim = false
@@ -139,6 +223,7 @@ func _animate_rotation() -> bool:
 	else:
 		rotation += deg_to_rad(this_frame_rotation)
 	$sprite_unit.rotation = -rotation
+	$RigidUI.rotation = -rotation
 
 
 	if abs(fposmod(rotation_degrees, 360) - _target_rotation_degrees) < 0.1:
@@ -181,63 +266,13 @@ func _animate_death(delta) -> bool:
 	scale.y = scale.x
 	return true
 
+#endregion Animations
+
+
+#region UI
 
 func set_selected(is_selected : bool):
 	var c = Color.RED if is_selected else Color.WHITE
 	$sprite_unit.modulate = c
 
-
-func apply_graphics(template : DataUnit, color : DataPlayerColor):
-	var unit_texture = load(template.texture_path) as Texture2D
-	_apply_unit_texture(unit_texture)
-	_apply_color_texture(color)
-	_apply_level_number(template.level)
-	for dir in range(0,6):
-		var symbol_texture = template.symbols[dir].texture_path
-		_apply_symbol_sprite(dir, symbol_texture)
-
-
-## WARNING: called directly in UNIT EDITOR
-func _apply_symbol_sprite(dir : int, texture_path : String) -> void:
-	var symbol_sprite = $"Symbols".get_children()[dir].get_child(0).get_child(0)
-	if texture_path == null or texture_path.is_empty():
-		symbol_sprite.texture = null
-		symbol_sprite.hide()
-		return
-	symbol_sprite.texture = load(texture_path)
-
-	_flip_symbol_sprite(symbol_sprite, dir)
-
-	symbol_sprite.show()
-
-
-## Flips ths sprite so that weapons always point to the top of the screen
-func _flip_symbol_sprite(symbol_sprite : Sprite2D, dir : int):
-	var abstract_rotation : int = 0
-	if entity != null:
-		abstract_rotation = (entity.unit_rotation + dir) % 6
-	if abstract_rotation in [0, 1, 5]:  # LEFT
-		symbol_sprite.flip_v = false
-	else:
-		symbol_sprite.flip_v = true
-
-
-## WARNING: called directly in UNIT EDITOR
-func _apply_unit_texture(texture : Texture2D) -> void:
-	$sprite_unit.texture = texture
-
-
-func _apply_color_texture(color : DataPlayerColor) -> void:
-	var color_texture_name : String = color.hexagon_texture
-	var path = "res://Art/player_colors/%s.png" % color_texture_name
-	var texture = load(path) as Texture2D
-	assert(texture, "failed to load background " + path)
-	$sprite_color.texture = texture
-
-
-func _apply_level_number(level : int) -> void:
-	const roman_numbers = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
-	if level > 10 or level < 0:
-		assert(false, "Design wise higher level units don't make sense")
-		level = 1
-	$sprite_unit/UnitLevel.text = roman_numbers[level]
+#endregion UI
