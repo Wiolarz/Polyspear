2420bab217de8614d40bb596ef65233d462ef985
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2024-12-22 18:01:26 +0100

    new temp higher quality graphics

--- Scripts/General/grid_node2d.gd
+++ Scripts/General/grid_node2d.gd
@@ -5,10 +5,13 @@ extends Node2D
 ## E.g. for visualising grid data, handling click events etc
 
 const VISUAL_EMPTY_BORDER = 11.0 # Hex Sprite draw gaps
-const TILE_OFFSET_HORIZONTAL_PER_X : float = 529.0 + VISUAL_EMPTY_BORDER # current sprite size 529
-const TILE_OFFSET_HORIZONTAL_PER_Y : float = TILE_OFFSET_HORIZONTAL_PER_X / 2
-const TILE_OFFSET_VERTICAL_PER_Y : float = (608 + VISUAL_EMPTY_BORDER) * 0.75
+#const TILE_OFFSET_HORIZONTAL_PER_X : float = 529.0 + VISUAL_EMPTY_BORDER # current sprite size 529
+#const TILE_OFFSET_HORIZONTAL_PER_Y : float = TILE_OFFSET_HORIZONTAL_PER_X / 2
+#const TILE_OFFSET_VERTICAL_PER_Y : float = (608 + VISUAL_EMPTY_BORDER) * 0.75
 
+const TILE_OFFSET_HORIZONTAL_PER_X : float = (529 * 3.2) + VISUAL_EMPTY_BORDER # current sprite size 529
+const TILE_OFFSET_HORIZONTAL_PER_Y : float = TILE_OFFSET_HORIZONTAL_PER_X / 2
+const TILE_OFFSET_VERTICAL_PER_Y : float = (608 * 3.2 + VISUAL_EMPTY_BORDER) * 0.75
 
 func to_position(coord : Vector2i) -> Vector2:
 	var horizontal = coord.x * TILE_OFFSET_HORIZONTAL_PER_X
