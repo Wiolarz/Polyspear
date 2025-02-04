df511978a50c7b8c673ade111fd8aa4b9f039ebc
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2025-02-04 15:03:39 +0100

    Removed unused Preset Player, our test approach to world gameplay will be different

---
+++ Resources/Presets/preset_world.gd
@@ -0,0 +1,7 @@
+class_name PresetWorld
+extends Resource
+
+
+@export_category("Main")
+#@export var player_settings : Array = []  # TODO develop a better way of creating world map scenarios over player preset
+@export var world_map : DataWorldMap
