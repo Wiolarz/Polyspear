ceeed859b2e796b0e8f2d8214eb5cb3bc365e762
Michał Moniewski <76787997+carcinizer@users.noreply.github.com>
2025-02-17 00:11:19 +0100

    Refactor/improve settings (#141)
    * Refactor/improve settings + better documentation for those who want to add more
    
    * Change player options defaults

--- Resources/player_options.gd
+++ Resources/player_options.gd
@@ -1,26 +1,39 @@
 class_name PlayerOptions
 extends Resource
 
+
+## How to add settings:
+## 1. add an export variable here
+## 2. add an appropriate (as in _declare_* functions in settings_menu.gd) 
+##    control to SettingsMenu.tscn
+## 3. add a "_declare_*" call in settings_menu.gd's _ready function,
+##    referencing the added control
+
+
 ## once the game is launched it instantly start the game with default lobby values [br]
 ## depending on "use_default_battle" value starts either World/Battle
-@export var autostart_map : bool
+@export var autostart_map : bool = false
 
 ## Default starting screen when launching a game (autostart_map uses this variable)
-@export var use_default_battle : bool
+@export var use_default_battle : bool = true
 ## if true all player slots are placed in control of AI controller [br]
 ## if false all slots are by default controlled by host (human player)
-@export var use_default_AI_players : bool
+@export var use_default_AI_players : bool = false
+
+## if true, compares BattleGridState and LibSpear's BattleManagerFast
+## for mismatches before and after each battle move, activating an assert on mismatch
+@export var bmfast_integrity_checks : bool = true
 
 ## if true, peer ip addresses are hidden
-@export var streamer_mode : bool
+@export var streamer_mode : bool = false
 
 ## enable fancy, but distracting background color change following player turns
-@export var background_color_follows_players : bool
+@export var background_color_follows_players : bool = true
 
 ## choose type of gui animation, non-distraction means that some in-game animation
 ## which look nice but can distract a player are disabled
 ## STUB
-@export var gui_animation_mode : CFG.GuiAnimationMode
+@export var gui_animation_mode := CFG.GuiAnimationMode.FULL
 
 ## Default first option after opening the game should be last selected one
 ## [br]
@@ -29,6 +42,12 @@ extends Resource
 
 ## Default first option after opening the game should be last selected one
 @export var last_used_world_preset : PresetWorld
+
+@export var fullscreen : bool:
+	set(new):
+		if new != fullscreen:
+			(func(): UI.set_fullscreen(new)).call_deferred()
+		fullscreen = new
 
 @export var login : String
 ## if true adds a random number suffix at the end of a login string
--- Scripts/UI/settings_menu.gd
+++ Scripts/UI/settings_menu.gd
@@ -1,78 +1,80 @@
 extends Control
 
+## Refer to player_controls.gd for information on adding new settings
+
+func _ready():
+	# You should put all PlayerOptions-widget connections here
+	_declare_toggle("autostart_map", $ToggleAutoStart)
+	_declare_toggle("use_default_battle", $ToggleBattleDefault)
+	_declare_toggle("use_default_AI_players", $ToggleDefaultAIPlayers)
+	_declare_toggle("streamer_mode", $ToggleStreamerMode)
+	_declare_toggle("fullscreen", $ToggleFullscreen)
+	_declare_toggle("bmfast_integrity_checks", $ToggleBMFastIntegrityChecks)
+	_declare_toggle("background_color_follows_players", $ToggleBackgroundColorFollowsPlayers)
+	_declare_enum_list("gui_animation_mode", $OptionGuiAnimationMode/Option, {
+		CFG.GuiAnimationMode.NONE: "None",
+		CFG.GuiAnimationMode.NON_DISTRACTION: "Only non-distracting",
+		CFG.GuiAnimationMode.FULL: "All"
+	})
+
+#region Widgets
+#region - Toggle
+
+## Connect a given option with a widget
+## Use as a template for other widgets
+func _declare_toggle(option : StringName, node : CheckBox):
+	# Immediately update option
+	node.button_pressed = CFG.player_options.get(option)
+	
+	# Refresh button visuals when a setting is changed
+	UI.update_settings.connect(func(): 
+		# Lambda instead of bind because one callable can be only bound to signal once
+		node.button_pressed = CFG.player_options.get(option)
+	)
+	
+	# Bind a toggle press
+	node.pressed.connect(func():
+		# This one could be a dedicated func, but for consistency let's keep it that way
+		var old_option = CFG.player_options.get(option)
+		CFG.player_options.set(option, not old_option)
+		CFG.save_player_options()
+		UI.update_settings.emit()
+	)
+
+#endregion - Toggle
 
 
-@onready var main_container = $Margin/VBox
+#region - Enum List
 
-@onready var toggle_auto_start = main_container.get_node("ToggleAutoStart")
-@onready var toggle_battle_default = main_container.get_node("ToggleBattleDefault")
-@onready var toggle_default_ai_players = main_container.get_node("ToggleDefaultAIPlayers")
-@onready var toggle_streamer_mode = main_container.get_node("ToggleStreamerMode")
-@onready var toggle_background_color_follows_players = \
-	main_container.get_node("ToggleBackgroundColorFollowsPlayers")
-@onready var cycle_gui_anim_mode : Button = main_container.get_node("CycleGuiAnimationMode")
-
-
-func refresh():
-	toggle_auto_start.button_pressed = CFG.AUTO_START_GAME
-	toggle_battle_default.button_pressed = CFG.DEFAULT_MODE_IS_BATTLE
-	toggle_default_ai_players.button_pressed = CFG.player_options.use_default_AI_players
-	toggle_streamer_mode.button_pressed = CFG.player_options.streamer_mode
-	toggle_background_color_follows_players.button_pressed = \
-		CFG.player_options.background_color_follows_players
-
-	var anim_mode = CFG.player_options.gui_animation_mode
-	cycle_gui_anim_mode.text = "GUI animation mode: %s" % (
-		"none" if anim_mode == CFG.GuiAnimationMode.NONE else
-		"only non-distracting" if \
-			anim_mode == CFG.GuiAnimationMode.NON_DISTRACTION else
-		"all"
+## Connect enum variable to an option button
+func _declare_enum_list(enum_var : StringName, node : OptionButton, value_mappings : Dictionary):
+	var id_enum_mappings := {}
+	var enum_id_mappings := {}
+	
+	var c := -1
+	for enum_item in value_mappings:
+		c += 1
+		node.add_item(value_mappings[enum_item])
+		id_enum_mappings[c] = enum_item
+		enum_id_mappings[enum_item] = c
+	
+	node.selected = enum_id_mappings[CFG.player_options.get(enum_var)]
+	
+	# Refresh visuals when a setting is changed
+	UI.update_settings.connect(func(): 
+		# Lambda instead of bind because one callable can be only bound to signal once
+		var enum_value = CFG.player_options.get(enum_var)
+		node.selected = enum_id_mappings[enum_value]
+	)
+	
+	# Bind an item select
+	node.item_selected.connect(func(index : int):
+		CFG.player_options.set(enum_var, id_enum_mappings[index])
+		CFG.save_player_options()
+		UI.update_settings.emit()
 	)
 
 
-func _on_toggle_auto_start_pressed():
-	# TODO refactor code copying
-	CFG.player_options.autostart_map = not CFG.player_options.autostart_map
-	CFG.save_player_options()
-	toggle_auto_start.button_pressed = CFG.AUTO_START_GAME
-
-
-func _on_toggle_battle_default_pressed():
-	CFG.player_options.use_default_battle = not CFG.player_options.use_default_battle
-	CFG.save_player_options()
-	toggle_battle_default.button_pressed = CFG.DEFAULT_MODE_IS_BATTLE
-
-
-func _on_toggle_background_color_follows_players():
-	CFG.player_options.background_color_follows_players = \
-		not CFG.player_options.background_color_follows_players
-	CFG.save_player_options()
-	toggle_background_color_follows_players.button_pressed = \
-		CFG.player_options.background_color_follows_players
-
-
-func _on_toggle_auto_start_visibility_changed():
-	if visible:
-		refresh()
-
-
-func _on_toggle_default_ai_players_pressed():
-	#TODO quick refactor
-	CFG.player_options.use_default_AI_players = not CFG.player_options.use_default_AI_players
-	CFG.save_player_options()
-	toggle_default_ai_players.button_pressed = CFG.player_options.use_default_AI_players
-
-
-func _on_toggle_streamer_mode_pressed():
-	CFG.player_options.streamer_mode = not CFG.player_options.streamer_mode
-	CFG.save_player_options()
-	toggle_streamer_mode.button_pressed = CFG.player_options.streamer_mode
-
-
-func _on_cycle_gui_anim_mode_pressed():
-	var mode : int = CFG.player_options.gui_animation_mode + 1
-	if mode >= CFG.GuiAnimationMode.MAX_:
-		mode = CFG.GuiAnimationMode.NONE
-	CFG.player_options.gui_animation_mode = mode as CFG.GuiAnimationMode
-	refresh()
+#endregion - Enum List
+#endregion Widgets
 
