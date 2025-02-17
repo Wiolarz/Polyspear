686ec4df6e00413625cc89ec26df6fe00de25d5f
Zimny <szpa.general@gmail.com>
2025-02-17 23:11:43 +0100

    Complex Symbol Animations Established

--- Resources/Battle/symbol_animation.gd
+++ Resources/Battle/symbol_animation.gd
@@ -1,6 +1,5 @@
 class_name SymbolAnimation
 extends SpriteFrames
 
-@export var x_offset : int = 0
-@export var y_offset : int = 0
+@export var offset : Vector2 = Vector2(0,0)
 @export var scale : Vector2 = Vector2(1,1)
