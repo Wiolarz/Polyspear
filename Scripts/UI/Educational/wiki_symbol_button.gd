874d42e265165485aed9b050b2bde24a3c510456
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2025-06-25 21:40:44 +0200

    Working Symbols Wiki

--- Scripts/UI/Educational/wiki_symbol_button.gd
+++ Scripts/UI/Educational/wiki_symbol_button.gd
@@ -1,28 +1,19 @@
-extends Panel
+class_name WikiSymbolButton
+extends TextureButton
 
-@onready var button_columns : Array[VBoxContainer] = [ \
-	$Margin/VBoxContainer/HBoxContainer/Scroll/VBox/HBoxContainer/Column1,
-	$Margin/VBoxContainer/HBoxContainer/Scroll/VBox/HBoxContainer/Column2,
-	$Margin/VBoxContainer/HBoxContainer/Scroll/VBox/HBoxContainer/Column3]
+var symbol : DataSymbol
+
+signal selected(symbol)
+
+func _on_pressed():
+	selected.emit(symbol)
 
 
-func _ready():
-	generate_data_symbol_buttons()
+func load_symbol(symbol_ : DataSymbol) -> void:
+		symbol = symbol_
+		get_node("Label").text = symbol.symbol_name
 
-
-func generate_data_symbol_buttons() -> void:
-	# clean mockup ui
-	for column in button_columns:
-		for mock_button in column.get_children():
-			mock_button.queue_free()
-
-	var path = CFG.SYMBOLS_PATH
-	var dir = DirAccess.open(path)
-	var symbol_idx : int = -1
-	for data_symbol_file_path in dir.get_files():
-		symbol_idx += 1
-		var data_symbol : DataSymbol = load(path + data_symbol_file_path)
-
-		var button : TextureButton = load("res://Scenes/UI/Wiki/wiki_symbol_button.tscn").instantiate()
-
-		button_columns[symbol_idx % button_columns.size()].add_child(button)
+		if symbol.symbol_name == "empty":
+			texture_normal = null
+			return
+		texture_normal = load(symbol.texture_path)
