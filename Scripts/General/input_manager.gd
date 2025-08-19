a49ef27e11ecca037eb8425e73a381ecb96dea8b
Trupen <MrTrupen@gmail.com>
2025-08-19 22:36:20 +0200

    rename discord manager autoload

--- Scripts/General/input_manager.gd
+++ Scripts/General/input_manager.gd
@@ -90,7 +90,7 @@ func start_game(world_state : SerializableWorldState = null,
 
 		_start_game_battle(battle_state, replay_template)
 		UI.set_camera(E.CameraPosition.BATTLE)
-		DM.change_state("Playing custom battle")
+		DISCORD.change_state("Playing custom battle")
 
 	elif game_setup_info.is_in_mode_world():
 		# in world mode we can have no states, only world state or both states
@@ -105,7 +105,7 @@ func start_game(world_state : SerializableWorldState = null,
 			for army_coord in battle_state.world_armies:
 				armies.append(WS.get_army_at(army_coord))
 			WM.start_combat(armies, battle_state.combat_coord, battle_state)
-		DM.change_state("Exploring the world map")
+		DISCORD.change_state("Exploring the world map")
 
 	if NET.server:
 		NET.server.broadcast_start_game()
