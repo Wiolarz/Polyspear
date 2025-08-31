d1de4d7fe7bb232b6ff39bc8232304a8a2bc2736
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2025-08-31 02:12:27 +0200

    barebones world terrain wiki

---
+++ Scripts/UI/Educational/WorldWiki/wiki_world_tile_button.gd
@@ -0,0 +1,17 @@
+class_name WikiTerrainButton
+extends TextureButton
+
+var tile : DataTile
+
+signal selected(tile)
+
+func _on_pressed():
+	selected.emit(tile)
+
+
+func load_tile(tile_ : DataTile) -> void:
+		tile = tile_
+
+		get_node("Label").text = tile.type
+
+		texture_normal = load(tile.texture_path)
